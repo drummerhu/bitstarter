56569575f32ce68ecee0d50a4df5837f7c32215f
Lorant Dobos <drummer@drummer.hu>
2013-07-10 22:28:09 +0000

    final hw3-3

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,26 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(htmlfile, cb) {
+    fs.readFile(htmlfile, function(e, s) {
+	if (e)
+	    throw e;
+	cb(s);
+    });
+};
+
+var cheerioHtmlRemote = function(url, cb) {
+    restler.get(url).on('complete', function(s) {
+	cb(s);
+    });
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlString = function(html, checksfile) {
+    var $ = cheerio.load(html);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +76,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var cb = function(s) {
+	var checkJson = checkHtmlString(s, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    };
+    if (program.url)
+	cheerioHtmlRemote(program.url, cb);
+    else
+	cheerioHtmlFile(program.file, cb);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
